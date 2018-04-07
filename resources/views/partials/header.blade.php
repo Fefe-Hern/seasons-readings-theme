1c4b4924fd84d5ac48620b3e98a58c250e70072f
FefeHern <fefehern@hotmail.com>
2018-04-07 02:10:09 -0400

    Equalized header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,26 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
+  <div class="site-header-sections page-width sr-flex">
+    <div class="site-header__section menu-text h4 sr-flex-child">
+    	<a href="https://seasonsreadings.net" itemprop="url" class=""><span>Home</span></a>
+    </div>
+    <!-- Logo Begin -->
+    <div class="site-header__section site-header__section--title sr-flex-child">
+        <div class="site-header__logo-wrapper h4" itemscope="" itemtype="http://schema.org/Organization">
+          <a href="/" itemprop="url" class="site-header__logo">
+            <style>
+              .site-header__logo-image {
+                height: 64px;
+              }
+            </style>
+
+            <img class="site-header__logo-image sr-logo" src="//cdn.shopify.com/s/files/1/2993/0170/files/logo_8335f0ae-9a1a-4ba0-b9e6-1b3072b1d23b_x64.png?v=1521049611" srcset="//cdn.shopify.com/s/files/1/2993/0170/files/logo_8335f0ae-9a1a-4ba0-b9e6-1b3072b1d23b_x64.png?v=1521049611 1x, //cdn.shopify.com/s/files/1/2993/0170/files/logo_8335f0ae-9a1a-4ba0-b9e6-1b3072b1d23b_x64@2x.png?v=1521049611 2x" alt="Season's Readings" itemprop="logo" />  
+          </a>
+        </div>    
+    </div>
+    <!-- Logo end -->
+
+    <div class="site-header__section menu-text h4 sr-flex-child">
+    	<a href="https://seasonsreadings.net/blogs/reading" itemprop="url" class=""><span>Blog</span></a>
+    </div>
   </div>
 </header>
