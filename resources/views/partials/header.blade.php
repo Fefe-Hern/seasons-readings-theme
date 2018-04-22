cf1e00880c368d0c77a4a71a6ebcff756391846d
FefeHern <fefehern@hotmail.com>
2018-04-22 14:57:04 -0400

    Changing to desktop

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,12 +7,6 @@
     <div class="site-header__section site-header__section--title sr-flex-child">
         <div class="site-header__logo-wrapper h4" itemscope="" itemtype="http://schema.org/Organization">
           <a href="/" itemprop="url" class="site-header__logo">
-            <style>
-              .site-header__logo-image {
-                height: 64px;
-              }
-            </style>
-
             <img class="site-header__logo-image sr-logo" src="//cdn.shopify.com/s/files/1/2993/0170/files/logo_8335f0ae-9a1a-4ba0-b9e6-1b3072b1d23b_x64.png?v=1521049611" srcset="//cdn.shopify.com/s/files/1/2993/0170/files/logo_8335f0ae-9a1a-4ba0-b9e6-1b3072b1d23b_x64.png?v=1521049611 1x, //cdn.shopify.com/s/files/1/2993/0170/files/logo_8335f0ae-9a1a-4ba0-b9e6-1b3072b1d23b_x64@2x.png?v=1521049611 2x" alt="Season's Readings" itemprop="logo" />  
           </a>
         </div>    
