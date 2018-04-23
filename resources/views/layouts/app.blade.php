349c9d6f91e6338608924ee63ccefdffa3fc9454
Fernando Hernandez <fhernandez@ivionics.com>
2018-04-23 16:53:08 -0400

    matched post functionality to shopify'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,22 +2,24 @@
 <html @php(language_attributes())>
   @include('partials.head')
   <body @php(body_class())>
-    @php(do_action('get_header'))
-    @include('partials.header')
-    <div class="wrap container" role="document">
-      <div class="content">
-        <main class="main">
-          @yield('content')
-        </main>
-        @if (App\display_sidebar())
-          <aside class="sidebar">
-            @include('partials.sidebar')
-          </aside>
-        @endif
+    <div class='site'>
+      @php(do_action('get_header'))
+      @include('partials.header')
+      <div class="wrap max" role="document">
+        <div class="content max">
+          <main class="main max">
+            @yield('content')
+          </main>
+          @if (App\display_sidebar())
+            <aside class="sidebar">
+              @include('partials.sidebar')
+            </aside>
+          @endif
+        </div>
       </div>
+      @php(do_action('get_footer'))
+      @include('partials.footer')
+      @php(wp_footer())
     </div>
-    @php(do_action('get_footer'))
-    @include('partials.footer')
-    @php(wp_footer())
   </body>
 </html>
--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -1,15 +1,17 @@
 <article @php(post_class())>
-  <div class="sr-post">
-    <header class="article-header">
-      @include('partials/entry-meta')
-      <h1 class="article-title">{{ get_the_title() }}</h1>
-    </header>
-    <div class="sr-article">
-      @php(the_content())
+  <div class="article-wrapper max">
+    <div class="sr-post max">
+      <header class="article-header">
+        @include('partials/entry-meta')
+        <h2 class="article-title">{{ get_the_title() }}</h2>
+      </header>
+      <div class="sr-article">
+        @php(the_content())
+      </div>
+      <footer>
+        {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
+      </footer>
+      @php(comments_template('/partials/comments.blade.php'))
     </div>
-    <footer>
-      {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
-    </footer>
-    @php(comments_template('/partials/comments.blade.php'))
   </div>
 </article>
