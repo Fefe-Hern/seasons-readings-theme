7f9c714385c9b506406dc040b846e170cf5c5501
Fernando Hernandez <fhernandez@ivionics.com>
2018-04-24 12:53:50 -0400

    Added footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,9 @@
 <footer class="content-info">
-  <div class="container">
-    @php(dynamic_sidebar('sidebar-footer'))
+  <div class="container sr-footer">
+    {{--@php(dynamic_sidebar('sidebar-footer'))--}}
+    <div class="copyright-sr">
+      <small>Copyright © 2018, </small>
+      <a href="/" title="">Season's Readings</a>
+    </div>
   </div>
 </footer>
