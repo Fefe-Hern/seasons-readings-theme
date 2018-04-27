9186f144c4883c851a5de4d0239946530bd8daf8
Fernando Hernandez <fhernandez@ivionics.com>
2018-04-27 16:48:13 -0400

    Adding index features. Undone

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,9 +1,17 @@
-<article @php(post_class())>
-  <header>
-    <h2 class="entry-title"><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h2>
-    @include('partials/entry-meta')
-  </header>
-  <div class="entry-summary">
-    @php(the_excerpt())
-  </div>
-</article>
+<div class="sr-card">
+  <a href="{{ get_permalink() }}" class="card-wrapper">
+    <div class="card-image-wrapper" style="">
+      <div class="card-preloader"></div>
+      <img src="<?=the_post_thumbnail_url(array(450, 0))?>" alt="" class="card-image fade-in" />
+    </div>
+    <div class="card-info">
+      <span class="card-date">@include('partials/entry-meta')</span>
+      <h3 class="card-title">{{ get_the_title() }}</h2>
+      
+      <div class="card-summary">
+        @php(the_excerpt())
+      </div>
+    <span class="read-more">Read More <svg aria-hidden="true" focusable="false" role="presentation" class="icon icon-arrow-right" viewBox="0 0 4 7"><path fill="#444" d="M4 3.5L0 7V0z"></path></svg></span>
+    </div>
+  </a>
+</div>
