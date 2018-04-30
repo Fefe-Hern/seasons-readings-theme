082242e71c1246583d6a5dee5c20f89285034258
Fernando Hernandez <fhernandez@ivionics.com>
2018-04-30 13:56:52 -0400

    add cards

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="page-width">
+    @include('partials.page-header')
+
+    @if (!have_posts())
+      <div class="alert alert-warning">
+        {{ __('Sorry, no results were found.', 'sage') }}
+      </div>
+      {!! get_search_form(false) !!}
+    @endif
+
+    <div class="card-list sr_flex flex_spaced">
+      @while (have_posts()) @php(the_post())
+      @include('partials.content-'.get_post_type())
+      @endwhile
+    </div>
+    {!! get_the_posts_navigation() !!}
+  </div>
+@endsection
