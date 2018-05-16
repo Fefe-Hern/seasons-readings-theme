ebc77bb84e53b30bd8ebf04213ec4273b02a617d
Fernando Hernandez <fhernandez@ivionics.com>
2018-05-16 16:33:26 -0400

    URL Changes

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,7 +3,7 @@
     {{--@php(dynamic_sidebar('sidebar-footer'))--}}
     <div class="copyright-sr">
       <small>Copyright © 2018, </small>
-      <a href="/" title="">Season's Readings</a>
+      <a href="https://www.seasonsreadings.net" title="">Season's Readings</a>
     </div>
   </div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,14 +7,14 @@
     <div class="site-header__section site-header__section--title sr-flex-child">
         <div class="site-header__logo-wrapper h4" itemscope="" itemtype="http://schema.org/Organization">
           <a href="/" itemprop="url" class="site-header__logo">
-            <img class="site-header__logo-image sr-logo" src="//cdn.shopify.com/s/files/1/2993/0170/files/logo_8335f0ae-9a1a-4ba0-b9e6-1b3072b1d23b_x64.png?v=1521049611" srcset="//cdn.shopify.com/s/files/1/2993/0170/files/logo_8335f0ae-9a1a-4ba0-b9e6-1b3072b1d23b_x64.png?v=1521049611 1x, //cdn.shopify.com/s/files/1/2993/0170/files/logo_8335f0ae-9a1a-4ba0-b9e6-1b3072b1d23b_x64@2x.png?v=1521049611 2x" alt="Season's Readings" itemprop="logo" />  
+            <img class="site-header__logo-image sr-logo" src="https://blog.seasonsreadings.net/wp-content/uploads/2018/05/logo-small.png" alt="Season's Readings" itemprop="logo" />  
           </a>
         </div>    
     </div>
     <!-- Logo end -->
 
     <div class="site-header__section menu-text h4 sr-flex-child">
-    	<a href="https://seasonsreadings.net/blogs/reading" itemprop="url" class=""><span>Blog</span></a>
+    	<a href="https://blog.seasonsreadings.net" itemprop="url" class=""><span>Blog</span></a>
     </div>
   </div>
 </header>
