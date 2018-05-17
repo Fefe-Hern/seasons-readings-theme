5a1cdbbdfb1b901b2be9845b71cff959c515a895
Fernando Hernandez <fhernandez@ivionics.com>
2018-05-17 15:47:47 -0400

    added proper color

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,7 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="theme-color" content="#ffe54c">
+  <meta name="theme-color" content="#61d174">
   <link href="//fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet" type="text/css" media="all" />
   <link href="//fonts.googleapis.com/css?family=Droid+Serif:400" rel="stylesheet" type="text/css" media="all" />
   
