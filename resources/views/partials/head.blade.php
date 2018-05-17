3349d0022b176c528ca77abe18bd47f613668513
Fernando Hernandez <fhernandez@ivionics.com>
2018-05-17 15:47:05 -0400

    Added color to mobile tab

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,6 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="theme-color" content="#ffe54c">
   <link href="//fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet" type="text/css" media="all" />
   <link href="//fonts.googleapis.com/css?family=Droid+Serif:400" rel="stylesheet" type="text/css" media="all" />
   
